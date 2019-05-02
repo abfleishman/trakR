855e9d7eb3342e42508aa6ce727572e2498a5e58
Abram Fleishman <abram@conservationmetrics.com>
2019-05-01 21:29:04 -0700

    document tracks

---
+++ R/tracks.R
@@ -0,0 +1,16 @@
+#' tracks from a red-legged kittiwake
+#'
+#' this dataset is a not big...
+#'
+#' @author Abram B. Fleishman <abram.fleishman AT sjsu.edu>
+#'
+#' @format A data frame with 2964 rows and 5 variables:
+#' \describe{
+#'   \item{DateTime}{datetime in yyy-mm-dd hh:mm:ss}
+#'   \item{Longitude}{longitude}
+#'   \item{Latitude}{latitude}
+#'   \item{CaptureID}{bird capture id}
+#'   \item{Dist2Colony}{distance in km from the colony}
+#' }
+#'
+"tracks"
