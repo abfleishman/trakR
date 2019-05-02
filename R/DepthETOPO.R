93db63d7e682a96a5cb35841d96b08c779a6945a
Abram Fleishman <abram@conservationmetrics.com>
2019-05-01 21:29:17 -0700

    rename addDepth

--- R/DepthETOPO.R
+++ R/DepthETOPO.R
@@ -1,6 +1,9 @@
 #' DepthETOPO Compute the depth from Etopo1 is at 1 arc minute resolution.
+#'
 #' It would be nice to find something higher resolution
+#'
 #' @author Abram B. Fleishman <abram.fleishman AT sjsu.edu>
+#'
 #' @param tracks data.frame of data to be queried for  latand long values
 #' @param dataLon quoted name of column in data that has longitude values
 #' @param dataLat quoted name of column in data that has latitude values
@@ -10,13 +13,12 @@
 #' @param bottom The bottom side of the bounding box of data
 #' @param resolution arc minutes res
 #' @param keep save a file
+#'
 #' @return A vector of elevation/depth in meters from ETOPO1 world bathymatry at a resolution of 1 arc minute
+#'
 #' @export
-
-
-# Etopo1 is at 1 arc minute resolution.  It would be nice to find something
-# higher resolution load track data tracks<-readRDS(data,
-# file = "processedDATA/GPSrlki_raw.rda")
+#' @importFrom marmap getNOAA.bathy as.SpatialGridDataFrame
+#' @importFrom SDMTools extract.data asc.from.sp
 
 DepthETOPO<-function(tracks,
                      dataLat="Latitude",
@@ -24,18 +26,15 @@ DepthETOPO<-function(tracks,
                      left=-179,right= -164,top=60,bottom=50,resolution = 30, keep=F){
 
   # Get bathymetric data ETOPO2
-  dat <- marmap::getNOAA.bathy(lon1 = right ,lon2 = left ,
+  dat <- getNOAA.bathy(lon1 = right ,lon2 = left ,
                                lat1 = bottom,lat2 = top ,
                                resolution = resolution, keep=keep)
 
-  SPData<-marmap::as.SpatialGridDataFrame(dat)
+  SPData<-as.SpatialGridDataFrame(dat)
 
-  depth<-SDMTools::extract.data(cbind(tracks[[dataLon]],
+  depth<-extract.data(cbind(tracks[[dataLon]],
                                       tracks[[dataLat]]),
-                                SDMTools::asc.from.sp(SPData))
+                                asc.from.sp(SPData))
   return(depth)
 }
 
-# tracks$depth<-DepthETOPO(data=tracks,dataLat="Latitude",
-# dataLon="Longitude",left=-179,right= -164,top=60,bottom=50)
-# head(as.data.frame(tracks),100)
