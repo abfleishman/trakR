f66a4c0deb40363a1f4797ae27f8516f05b52cdb
Abram B. Fleishman <abram@conservationmetrics.com>
2022-07-29 11:18:24 -0700

    force innterpoint time to be seconds

--- R/InterpointTime.R
+++ R/InterpointTime.R
@@ -5,9 +5,11 @@
 #' @author Abram B. Fleishman \email{abram@@conservationmetrics.com}
 #' @param tracks data.frame of data to be queried for lat and long values
 #' @param ID quoted name of column in data that is and ID field
-#' @param DateTime quoted name of column in data that has DateTime values in the format YYYY-mm-dd HH:MM:SS
-#' @importFrom dplyr lead
-#' @return A vector of times differnces in seconds between adjacent points in an animal track
+#' @param DateTime quoted name of column in data that has DateTime values in the
+#'   format YYYY-mm-dd HH:MM:SS
+#' @importFrom dplyr lead arrange
+#' @return A vector of times differences in seconds between adjacent points in
+#'   an animal track
 #' @export
 #############################################################################################
 # Calculate time between points in a timeseries
@@ -19,13 +21,16 @@ InterpointTime<-function(tracks, ID="File", DateTime="DateTime"){
   dataOut <- NULL
   Birds <- unique(tracks[[ID]])
 
-  # Run a for loop, where for each unique key, it subsets the data by that key and calculates the difference in time.
+  # Run a for loop, where for each unique key, it subsets the data by that key
+  # and calculates the difference in time.
   for(i in 1:length(Birds)) {
 
     Data<-tracks[tracks[[ID]] == Birds[i],]
-
+    Data<-arrange(Data,DateTime)
     Data$PointDur <- NA
-    Data$PointDur <- lead(Data[[DateTime]]) - Data[[DateTime]]
+    Data$PointDur <- as.numeric(difftime(time1 =  Data[[DateTime]],
+                                         time2 = lead(Data[[DateTime]]),
+                                         units = "secs"))
 
     dataOut<-c(dataOut,Data$PointDur)
   }
